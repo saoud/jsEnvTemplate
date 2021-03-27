56b6d2699cd9b6e3626e8699d14c93ff99bdde11
Saoud <415685+saoud@users.noreply.github.com>
2021-03-26 19:38:12 -0700

    adding missing dotenv in plugins as well.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-
 
 module.exports = {
   entry: './src/main.js',
@@ -10,18 +9,18 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'eval-source-map', 
-  devServer: {                 
-    contentBase: './dist'  
-  },    
+  devtool: 'eval-source-map',
+  devServer: {               
+    contentBase: './dist'    
+  },
   plugins: [
     new CleanWebpackPlugin(),
-    new Dotenv(),
     new HtmlWebpackPlugin({
-      title: 'Martian News Service',
+      title: 'Shape Tracker',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
@@ -33,28 +32,10 @@ module.exports = {
         ]
       },
       {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "eslint-loader"
-      },
-      {
-        test: /\.(gif|png|jpe?g)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/images/'
-            }
-          }
-        ]
-      },
-      {
-        test:/\.html$/,
-        use: [
-          'html-loader'
-        ]
-      },
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
     ]
   }
 };
